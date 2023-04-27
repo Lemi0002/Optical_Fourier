f15e7e269babe80d5fda90d3f063048caa0eaffa
Yanick Schoch <51885072+Lemi0002@users.noreply.github.com>
2023-04-27 14:08:14 +0200

    Test manim scripts

--- manim/main.py
+++ manim/main.py
@@ -1,10 +1,120 @@
 from manim import *
+from manimlib.imports import *
+from math import cos, sin, pi
 
 
 class TestClass(Scene):
     def construct(self):
         square = Square(side_length=5, stroke_color=GREEN,
                         fill_color=BLUE, fill_opacity=0.75)
+        equation = Tex(r'$\int\limits_{-\infty}^{\infty}f(x),dx$')
 
         self.play(Create(square), run_time=3)
         self.wait()
+        self.play(FadeOut(square))
+        self.play(FadeIn(equation))
+        self.wait()
+
+class TestClass1(Scene):
+    def construct(self):
+        axes = Axes(
+            x_range=[-3, 3],
+            y_range=[-5, 5],
+            axis_config={"color": BLUE},
+        )
+
+        # Create Graph
+        graph = axes.plot(lambda x: x**2, color=WHITE)
+        graph_label = axes.get_graph_label(graph, label='x^{2}')
+
+        graph2 = axes.plot(lambda x: x**3, color=WHITE)
+        graph_label2 = axes.get_graph_label(graph2, label='x^{3}')
+
+        # Display graph
+        self.play(Create(axes), Create(graph), Write(graph_label))
+        self.wait(1)
+        self.play(Transform(graph, graph2), Transform(graph_label, graph_label2))
+        self.wait(1)
+
+class TestClass2(Scene):
+    def construct(self):
+        axes = Axes(
+            x_range=[-3, 3, 1],
+            y_range=[-5, 5, 1],
+            x_length=10,
+            y_length=10,
+            axis_config={"color": BLUE},
+        )
+
+        self.play(Create(axes))
+
+class TestClass3(Scene):
+    def construct(self):
+        #######Code#######
+        #Making Shapes
+        circle = Circle(color=YELLOW)
+        square = Square(color=DARK_BLUE)
+        square.surround(circle)
+
+        rectangle = Rectangle(height=2, width=3, color=RED)
+        ring=Annulus(inner_radius=.2, outer_radius=1, color=BLUE)
+        ring2 = Annulus(inner_radius=0.6, outer_radius=1, color=BLUE)
+        ring3=Annulus(inner_radius=.2, outer_radius=1, color=BLUE)
+        ellipse=Ellipse(width=5, height=3, color=DARK_BLUE)
+
+        pointers = []
+        for i in range(8):
+            pointers.append(Line(ORIGIN, np.array([cos(pi/180*360/8*i),sin(pi/180*360/8*i), 0]),color=YELLOW))
+
+        #Showing animation
+        self.add(circle)
+        self.play(FadeIn(square))
+        self.play(Transform(square, rectangle))
+        self.play(FadeOut(circle), FadeIn(ring))
+        self.play(Transform(ring,ring2))
+        self.play(Transform(ring2, ring))
+        self.play(FadeOut(square), GrowFromCenter(ellipse), Transform(ring2, ring3))
+        self.add(*pointers)
+        self.wait(2)
+
+class TestClass4(Scene):
+    def construct(self):
+        #Making equations
+        first_eq = TextMobject("$$J(\\theta) = -\\frac{1}{m} [\\sum_{i=1}^{m} y^{(i)} \\log{h_{\\theta}(x^{(i)})} + (1-y^{(i)}) \\log{(1-h_{\\theta}(x^{(i)}))}] $$")
+        second_eq = ["$J(\\theta_{0}, \\theta_{1})$", "=", "$\\frac{1}{2m}$", "$\\sum\\limits_{i=1}^m$", "(", "$h_{\\theta}(x^{(i)})$", "-", "$y^{(i)}$", "$)^2$"]
+
+        second_mob = TextMobject(*second_eq)
+
+        for i,item in enumerate(second_mob):
+            if(i != 0):
+                item.next_to(second_mob[i-1],RIGHT)
+
+        eq2 = VGroup(*second_mob)
+
+        des1 = TextMobject("With manim, you can write complex equations like this...")
+        des2 = TextMobject("Or this...")
+        des3 = TextMobject("And it looks nice!!")
+
+        #Coloring equations
+        second_mob.set_color_by_gradient("#33ccff","#ff00ff")
+
+        #Positioning equations
+        des1.shift(2*UP)
+        des2.shift(2*UP)
+
+        #Animating equations
+        self.play(Write(des1))
+        self.play(Write(first_eq))
+        self.play(ReplacementTransform(des1, des2), Transform(first_eq, eq2))
+        self.wait(1)
+
+        for i, item in enumerate(eq2):
+            if (i<2):
+                eq2[i].set_color(color=PURPLE)
+            else:
+                eq2[i].set_color(color="#00FFFF")
+
+        self.add(eq2)
+        self.wait(1)
+        self.play(FadeOutAndShiftDown(eq2), FadeOutAndShiftDown(first_eq), Transform(des2, des3))
+        self.wait(2)
